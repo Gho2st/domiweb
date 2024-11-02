c4703633cc9b77d122698f413e60fa51cadb838d
Dominik <dominik.jojczyk@gmail.com>
2024-11-02 17:49:35 +0100

    Update page.js

--- app/pozycjonowanie/page.js
+++ app/pozycjonowanie/page.js
@@ -7,7 +7,16 @@ export default function Home() {
       <Nav />
       <main>
         <section className="p-6">
-          <h1>Pozycjonowanie nie jest takie straszne...</h1>
+          <h1 className="mt-6 text-3xl font-semibold">
+            Pozycjonowanie stron internetowych – dlaczego warto?
+          </h1>
+          <p className="my-10 text-neutral-900 text-lg font-light ">
+            Pozycjonowanie stron internetowych to proces, który sprawia, że
+            Twoja strona staje się widoczna w wynikach wyszukiwania Google.
+            Dzięki odpowiednim technikom SEO (ang. Search Engine Optimization)
+            strona może dotrzeć do nowych klientów, przyciągnąć większy ruch i
+            wyróżnić się na tle konkurencji.
+          </p>
         </section>
       </main>
       <Footer />
