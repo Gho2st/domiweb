9f06778c839dfdd1aee704b85e702ccf0ee059d1
Dominik <dominik.jojczyk@gmail.com>
2024-11-18 18:09:07 +0100

    Update page.js

--- app/portfolio/page.js
+++ app/portfolio/page.js
@@ -19,21 +19,21 @@ export default function Home() {
     {
       title: "Żłobki Wesołe Wygibasy",
       description:
-        "Nowoczesny projekt w Next.js dla krakowskiej firmy żłobkowej. Strona stworzona z myślą o łatwej nawigacji i dostępności dla rodziców. Design strony - pastelowe kolory + odpowiednia czcionka.",
+        "Strona internetowa dla krakowskiej sieci żłobków, zaprojektowana z myślą o rodzicach. Jasny, przyjazny design, intuicyjna nawigacja i pastelowa kolorystyka tworzą komfortowe doświadczenie użytkownika.",
       imageSrc: "/portfolio/wesolewygibasy.png",
       link: "https://www.wesolewygibasy.pl/",
     },
     {
       title: "Pub & Club Huki Muki",
       description:
-        "Dla krakowskiego pubu Huki Muki stworzyliśmy nowoczesną stronę w technologii Next.js, z integracją płatności Stripe i bazą danych PostgreSQL. Strona zawiera również system rezerwacji online.",
+        "Strona internetowa dla jednego z najpopularniejszych pubów w Krakowie. Zawiera system rezerwacji online, integrację płatności oraz dynamiczny design, który oddaje klimat miejsca.",
       imageSrc: "/portfolio/hukimuki.png",
       link: "https://www.hukimuki.pl/",
     },
     {
-      title: "Barbara Piękoś - Artystka.",
+      title: "Barbara Piękoś - Artystka",
       description:
-        "Strona internetowa dla Barbary Piękoś - artystki, która prezentuje swoje prace oraz najnowsze wystawy. Projekt oparty na minimalistycznym designie, by podkreślić jej artystyczny styl.",
+        "Strona, która eksponuje sztukę. Elegancki, minimalistyczny design podkreśla unikalny charakter twórczości Barbary, zapewniając jednocześnie czytelność i łatwą nawigację.",
       imageSrc: "/portfolio/barbarapiekos.png",
       link: "https://www.barbarapiekos.pl/",
     },
@@ -45,51 +45,51 @@ export default function Home() {
       link: "https://www.szlafroki.krakow.pl/",
     },
     {
-      title: "Stowarzyszenie hodowców ptaków",
+      title: "Stowarzyszenie Hodowców Ptaków",
       description:
-        "Dynamiczna strona internetowa z panelem administracyjnym stworzona w PHP i MySQL, która umożliwia łatwe zarządzanie treścią i użytkownikami, a także szybki dostęp do aktualizacji.",
+        "Dynamiczna witryna z panelem administracyjnym umożliwiającym zarządzanie treściami i użytkownikami. Idealne rozwiązanie dla organizacji potrzebujących szybkiego dostępu do aktualizacji.",
       imageSrc: "/portfolio/ptaki.png",
       link: "https://www.nshkipe.com.pl/",
     },
     {
       title: "Olmart - Zakład Zduński",
       description:
-        "Strona internetowa wykorzystująca Bootstrap i jQuery, prezentująca ofertę usług zduńskich, z naciskiem na SEO i widoczność w sieci.",
+        "Nowoczesna strona prezentująca usługi zduńskie. Prosty układ i optymalizacja pod kątem SEO pomagają w dotarciu do klientów poszukujących rzetelnych specjalistów.",
       imageSrc: "/portfolio/olmart.png",
       link: "https://www.olmart-ns.pl/",
     },
     {
       title: "Optyk - Andrzej Świerczek 1980",
       description:
-        "Stworzyliśmy responsywną stronę internetową dla lokalnego optyka, która została zaprojektowana w technologii Bootstrap. Dzięki optymalizacji SEO, strona zwiększyła widoczność w wynikach wyszukiwarek, a użytkownicy mogą łatwo umawiać wizyty online.",
+        "Strona lokalnego optyka z ponad 40-letnią tradycją. Dopracowany projekt wspiera pozycjonowanie w wyszukiwarkach, zwiększając zasięg firmy w regionie.",
       imageSrc: "/portfolio/optyk.png",
       link: "https://www.optyk-andrzej.pl/",
     },
     {
       title: "Beata Łakota",
       description:
-        "Profesjonalna strona internetowa stworzona z wykorzystaniem bootstrapa. Skoncentrowana na optymalizacji SEO i łatwej nawigacji.",
+        "Profesjonalna strona dla Beaty Łakoty, skoncentrowana na łatwej nawigacji i optymalizacji SEO, by dotrzeć do szerokiego grona odbiorców.",
       imageSrc: "/portfolio/beata.png",
       link: "https://www.beatalakota.pl/",
     },
     {
       title: "EOS - Ozonowanie | Kraków",
       description:
-        "Dla firmy EOS stworzyliśmy stronę z formularzem kontaktowym opartym na PHP Mailer, zoptymalizowaną pod kątem lokalnych usług ozonowania, co zwiększyło jej widoczność w wynikach wyszukiwania.",
+        "Strona internetowa zoptymalizowana pod kątem lokalnych usług ozonowania. Dzięki formularzowi kontaktowemu klienci mogą łatwo zgłosić zapytania i zamówić usługę.",
       imageSrc: "/portfolio/ozonowanie.png",
       link: "https://www.eos-ozonowanie.pl/",
     },
     {
       title: "Telmech - Obróbka Metali",
       description:
-        "Strona internetowa dla firmy Telmech zajmującej się obróbką metali, zoptymalizowana pod kątem SEO i używająca generowanych przez AI zdjęć.",
+        "Nowoczesna witryna dla firmy zajmującej się obróbką metali. Projekt wyróżnia się zastosowaniem AI w generowaniu grafik oraz optymalizacją SEO.",
       imageSrc: "/portfolio/telmech.png",
       link: "https://www.telmech.com/",
     },
     {
       title: "ToMy - Myje & Remontuje",
       description:
-        "Strona internetowa firmy remonoto-sprzątającej, zaprojektowana tak, aby umożliwić łatwy kontakt z klientami oraz przedstawienie oferty usług porządkowych.",
+        "Strona internetowa zaprojektowana z myślą o promocji usług porządkowych i remontowych. Przejrzysty układ ułatwia klientom szybkie nawiązanie kontaktu z firmą.",
       imageSrc: "/portfolio/tomy.png",
       link: "https://www.tomy-myje-remontuje.pl/",
     },
