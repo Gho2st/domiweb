4ff7df8ddd0b49f049a88ea4ae3e58043d475baa
Dominik <dominik.jojczyk@gmail.com>
2025-03-05 12:12:37 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,34 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      // Przekierowanie z HTTP na HTTPS (dla strony głównej i podstron)
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "header",
+            key: "x-forwarded-proto",
+            value: "http",
+          },
+        ],
+        destination: "https://www.domiweb.pl/:path*",
+        permanent: true,
+      },
+      // Przekierowanie z wersji bez "www" na wersję z "www" (dla strony głównej i podstron)
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "domiweb.pl", // Wersja bez "www"
+          },
+        ],
+        destination: "https://www.domiweb.pl/:path*",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
